f3a54e9d6ee1bdaadb578686263bc7150a836515
Bohdan Bura <bohdan.bura@teb.edu.pl>
2025-04-24 14:22:13 +0200

    Item class expansion

--- SystemOfStrangerThings/Program.cs
+++ SystemOfStrangerThings/Program.cs
@@ -1,10 +1,24 @@
-﻿
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Collections.Generic;
+using System;
 public class Item
 {
-   
-}
+    public string Name { get; set; }
+    public decimal Weight_kg { get; set; }
+    public int Weird_level { get; set; }
+    public bool Is_delicate { get; set; }
 
+    public Item(string name, decimal weight_kg, int weird_level, bool is_delicate)
+    {
+        Name = name;
+        Weight_kg = Math.Round(weight_kg, 3);
+        Weird_level = weird_level;
+        Is_delicate = is_delicate;
+    }
+}
 public class WareHouse
 {
 
 }
+
